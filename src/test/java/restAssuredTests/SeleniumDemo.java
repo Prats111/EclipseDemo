295fd72b60c4047d379d9f9047e3b962d204d006
Pratikshya <pratikshya069@gmail.com>
2025-04-11 09:21:30 +0530

    test

--- src/test/java/restAssuredTests/SeleniumDemo.java
+++ src/test/java/restAssuredTests/SeleniumDemo.java
@@ -28,36 +28,23 @@ public class SeleniumDemo {
 
 	    @Test
 	    public void setUp() throws MalformedURLException {
-//	    	 DesiredCapabilities capabilities = DesiredCapabilities();
-//	         capabilities.setCapability("platform", "Windows 10");
-//	         capabilities.setCapability("version", "latest");
-//	         capabilities.setCapability("name", "Sample Test");
+ 
+	    	 String username = System.getenv("SAUCE_USERNAME");
+	         String accessKey = System.getenv("SAUCE_ACCESS_KEY");
+	         String sauceURL = "https://" + username + ":" + accessKey + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+	         DesiredCapabilities capabilities = new DesiredCapabilities();
+	         capabilities.setBrowserName("chrome");
+	         capabilities.setCapability("platform", "Windows 10");
+	         capabilities.setCapability("version", "latest");
+	         capabilities.setCapability("name", "Sample Test");
+	         WebDriver driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
 
-	    	String username = System.getenv("SAUCE_USERNAME");
-	        String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-	        String sauceURL = "https://" + username + ":" + accessKey + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
-
-	    	ChromeOptions browserOptions = new ChromeOptions();
-	    	browserOptions.setPlatformName("Windows 8.1");
-	    	browserOptions.setBrowserVersion("latest");
-	    	Map<String, Object> sauceOptions = new HashMap<>();
-	    	sauceOptions.put("username", username);
-	    	sauceOptions.put("accessKey", accessKey);
-	    	sauceOptions.put("build", "23");
-	    	sauceOptions.put("name", "demo");
-	    	browserOptions.setCapability("sauce:options", sauceOptions);
-
-//	    	// start the session
-//    	URL url = new URL("https://oauth-pratikshya069-ebc02:8119ff65-a037-42a9-9ab2-c120a914e7a0@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
-    	RemoteWebDriver driver = new RemoteWebDriver(new URL(sauceURL), browserOptions);
-//	    	WebDriverManager.chromedriver().setup();
-//	    	WebDriver driver = new ChromeDriver();
-	        driver.manage().window().maximize();
+	         
+	    	  driver.manage().window().maximize();
 
 	        // Navigate to the application URL
 	        driver.get("https://www.google.com");
-	        System.out.println(driver.getTitle());
-	        System.out.flush(); 
+	        System.out.println("Title: " + driver.getTitle());
 	        driver.quit();	        
 	    }
 

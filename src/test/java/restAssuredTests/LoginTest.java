204df756494fb6c245b7115ab113b3e1b1bfbacb
Pratikshya <pratikshya069@gmail.com>
2025-04-17 18:47:44 +0530

    feature

---
+++ src/test/java/restAssuredTests/LoginTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testLoginPage() {
+        driver.get("https://example.com/login");
+        // your test steps
+        System.out.println("Login page test executed");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
